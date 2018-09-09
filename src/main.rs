8972eef28e6aa7b509ff21f705cd0ae8ef450211
Clément Bizeau <clement.bizeau4@gmail.com>
2018-09-09 14:02:39 +0200

    Get remperature very 2 seconds

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 #![no_std]
 
 extern crate cortex_m;
-#[macro_use(entry, exception)]
 extern crate cortex_m_rt as rt;
 extern crate cortex_m_semihosting as sh;
 extern crate f3;
@@ -10,16 +9,17 @@ extern crate panic_semihosting;
 
 use core::fmt::Write;
 
-use rt::ExceptionFrame;
+use rt::{entry, exception, ExceptionFrame};
 use sh::hio;
 
 use f3::hal::delay::Delay;
+use f3::hal::i2c::I2c;
 use f3::hal::prelude::*;
 use f3::hal::stm32f30x;
 use f3::led::Leds;
+use f3::Lsm303dlhc;
 
-entry!(main);
-
+#[entry]
 fn main() -> ! {
     let mut stdout = hio::hstdout().unwrap();
     writeln!(stdout, "Hello, world!").unwrap();
@@ -31,29 +31,36 @@ fn main() -> ! {
     let mut flash = dp.FLASH.constrain();
     let mut rcc = dp.RCC.constrain();
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
     let gpioe = dp.GPIOE.split(&mut rcc.ahb);
 
     let mut delay = Delay::new(cp.SYST, clocks);
     let mut leds = Leds::new(gpioe);
+
+    let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
+    let scl = gpiob.pb6.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
+    let sda = gpiob.pb7.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
+    let i2c1 = I2c::i2c1(dp.I2C1, (scl, sda), 400.khz(), clocks, &mut rcc.apb1);
+    let mut lsm = Lsm303dlhc::new(i2c1).unwrap();
 
     loop {
         for i in 0..leds.len() {
             let next = (i + 1) % leds.len();
             leds[i].off();
             leds[next].on();
-            delay.delay_ms(100_u16);
+            delay.delay_ms(2000_u16);
+            let temp = lsm.temp().unwrap();
+            writeln!(stdout, "temperature: {}", temp).unwrap();
         }
     }
 }
 
-exception!(HardFault, hard_fault);
-
-fn hard_fault(ef: &ExceptionFrame) -> ! {
+#[exception()]
+fn HardFault(ef: &ExceptionFrame) -> ! {
     panic!("HardFault at {:#?}", ef);
 }
 
-exception!(*, default_handler);
-
-fn default_handler(irqn: i16) {
+#[exception]
+fn DefaultHandler(irqn: i16) {
     panic!("Unhandled exception (IRQn = {})", irqn);
 }
